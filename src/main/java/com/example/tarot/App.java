43a7af593fd1743d2382de565b84b1e72a0dfbfa
PatkaPatka <patka.procelewska@gmail.com>
2025-06-20 14:00:16 +0200

    - changed from a Mac oriented design to a Windows orientated design (or in simpler terms: the design of the window is now the one that is set as default in IntelliJ) - programmed the method to allow the player window to be dragged across the screen - font size and card size change slightly upon maximising and minimising the screen - toggle bar as a different kind of help button for the "Spielhilfe" - changed button color - now there's a three split screen, having the cards and modes on the top, the buttons on the bottom and the "Spielhilfe" on the side with a help button

--- src/main/java/com/example/tarot/App.java
+++ src/main/java/com/example/tarot/App.java
@@ -7,20 +7,20 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.IOException;
+
 public class App extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        java.net.URL url = getClass().getResource("/view/MainView.fxml");
-        FXMLLoader loader = new FXMLLoader(url);
-        Parent root = loader.load();
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/mainView.fxml"));
+        Parent root = fxmlLoader.load();
 
         Scene scene = new Scene(root, 800, 600);
         scene.getStylesheets().add(getClass().getResource("/view/Style.css").toExternalForm());
 
-        primaryStage.setTitle("Tarot Reading App");
-        primaryStage.setScene(scene);
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.show();
+        stage.setScene(scene);
+        //stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,24 +1,29 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
-import javafx.scene.shape.Circle;
+import javafx.scene.layout.*;
 import model.*;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.input.MouseEvent;
 import java.util.List;
 
 public class MainController {
-    @FXML public Circle closeBtn, minimizeBtn, maximizeBtn;
+    @FXML public BorderPane rootPane;
+    @FXML public HBox titleBar;
     @FXML public Label time1Label, time2Label, time3Label;
     @FXML public Label meaning1Label, meaning2Label, meaning3Label;
     @FXML public ImageView card1Image, card2Image, card3Image;
-    @FXML public Button modeBtn, shuffleBtn, openBtn, helpBtn;
+    @FXML public Button modeBtn, shuffleBtn, openBtn, toggleHelpBtn;
+    @FXML public SplitPane mainSplit;
+    @FXML public TextArea helpText;
+    @FXML public ScrollPane helpPanel;
 
-    private boolean isMaximized = false;
+    private boolean isHelpVisible = true;
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     public TarotDeck tarotDeck;
     public ReadingMode currentMode;
@@ -29,80 +34,6 @@ public class MainController {
         currentMode = ReadingMode.past_present_future;
         updateModeLabels();
 
-        Image cardBack = new Image(getClass().getResourceAsStream("/images/resource/card_back.jpg"));
-        card1Image.setImage(cardBack);
-        card2Image.setImage(cardBack);
-        card3Image.setImage(cardBack);
-        card1Image.setFitHeight(200);
-        card1Image.setFitWidth(125);
-        card2Image.setFitHeight(200);
-        card2Image.setFitWidth(125);
-        card3Image.setFitHeight(200);
-        card3Image.setFitWidth(125);
-    }
-    @FXML
-    public void handleModeChange() {
-        ReadingMode[] modes = ReadingMode.values();
-        currentMode = modes[(currentMode.ordinal() + 1) % modes.length];
-        updateModeLabels();
-    }
-    public void updateModeLabels() {
-        String[] positions = currentMode.getPositions();
-        time1Label.setText(positions[0]);
-        time2Label.setText(positions[1]);
-        time3Label.setText(positions[2]);
-    }
-
-    @FXML
-    public void handleShuffle() {
-        tarotDeck.shuffle();
-        Image card_back = new Image(getClass().getResourceAsStream("/images/resource/card_back.jpg"));
-        card1Image.setImage(card_back);
-        card2Image.setImage(card_back);
-        card3Image.setImage(card_back);
-        meaning1Label.setText("");
-        meaning2Label.setText("");
-        meaning3Label.setText("");
-    }
-
-    @FXML
-    public void handleOpen() {
-        currentSpread = tarotDeck.drawSpread(3);
-
-        displayCard(card1Image, meaning1Label, currentSpread.get(0));
-        displayCard(card2Image, meaning2Label, currentSpread.get(1));
-        displayCard(card3Image, meaning3Label, currentSpread.get(2));
-    }
-
-    public void displayCard(ImageView imageView, Label meaningLabel, TarotCard card) {
-        try {
-            Image image = new Image(getClass().getResourceAsStream(card.getImagePath()));
-            imageView.setImage(image);
-            if (card.getReversed()) {
-                imageView.setRotate(180);
-            } else {
-                imageView.setRotate(0);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw e;
-        }
-
-        meaningLabel.setText(card.getName() +
-                (card.getReversed() ? " (Reversed)" : "") +
-                "\n" + card.getMeaning());
-    }
-
-    @FXML
-    public void showHelp() {
-        Stage helpStage = new Stage();
-        VBox root = new VBox();
-        root.setSpacing(10);
-        root.setPadding(new Insets(15));
-
-        TextArea helpText = new TextArea();
-        helpText.setEditable(false);
-        helpText.setWrapText(true);
         helpText.setText("How to play:\n\n" +
                 "1. Wählen Sie zunächst einen Modus, klicken Sie auf die Schaltfläche \"mode\", um den Modus zu ändern\n" +
                 "Vergangenheit, Gegenwart, Zukunft\n" +
@@ -115,36 +46,151 @@ public class MainController {
                 "3. Öffnen Sie die Karten (klicken Sie auf die Schaltfläche \"open\").\n\n" +
                 "4. Deuten Sie die Karten.\n\n");
 
-        Button closeBtn = new Button("Schließen");
-        closeBtn.setOnAction(e -> helpStage.close());
+        Image cardBack = new Image(getClass().getResourceAsStream("/images/resource/card_back.jpg"));
+        initializeCard(card1Image, cardBack);
+        initializeCard(card2Image, cardBack);
+        initializeCard(card3Image, cardBack);
 
-        root.getChildren().addAll(helpText, closeBtn);
-        Scene scene = new Scene(root, 400, 300);
-        helpStage.setScene(scene);
-        helpStage.setTitle("How to play");
-        helpStage.show();
-    }
+        // Set initial card sizes
+        updateCardSizes(120, 190, 14, 12);
 
-    @FXML
-    public void handleClose() {
-        ((Stage) closeBtn.getScene().getWindow()).close();
-    }
+        // Setup window dragging
+        if (titleBar != null) {
+            titleBar.setOnMousePressed(event -> {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            });
 
-    @FXML
-    public void handleMinimize() {
-        ((Stage) minimizeBtn.getScene().getWindow()).setIconified(true);
-    }
-
-    @FXML
-    public void handleMaximize() {
-        if (isMaximized) {
-            ((Stage) maximizeBtn.getScene().getWindow()).setMaximized(false);
-            ((Stage) maximizeBtn.getScene().getWindow()).setWidth(800);
-            ((Stage) maximizeBtn.getScene().getWindow()).setHeight(600);
+            titleBar.setOnMouseDragged(event -> {
+                Stage stage = (Stage) titleBar.getScene().getWindow();
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            });
         }
-        else {
-            ((Stage) maximizeBtn.getScene().getWindow()).setMaximized(true);
+
+        mainSplit.setDividerPosition(0, 0.85);
+
+        // Add listener for scene changes
+        rootPane.sceneProperty().addListener((observable, oldScene, newScene) -> {
+            if (newScene != null) {
+                setupWindowListeners(newScene);
+            }
+        });
+    }
+
+    private void setupWindowListeners(Scene scene) {
+        // Wait for window to be set
+        scene.windowProperty().addListener((obs, oldWindow, newWindow) -> {
+            if (newWindow != null) {
+                Stage stage = (Stage) newWindow;
+
+                // Listen to window maximized state
+                stage.maximizedProperty().addListener((obsMax, oldMax, newMax) -> {
+                    if (newMax) {
+                        updateCardSizes(180, 280, 16, 14);
+                    } else {
+                        updateCardSizes(120, 190, 14, 12);
+                    }
+                });
+
+                // Also listen to window size changes
+                stage.widthProperty().addListener((obsWidth, oldWidth, newWidth) -> {
+                    if (stage.isMaximized()) {
+                        updateCardSizes(180, 280, 16, 14);
+                    }
+                });
+
+                stage.heightProperty().addListener((obsHeight, oldHeight, newHeight) -> {
+                    if (stage.isMaximized()) {
+                        updateCardSizes(180, 280, 16, 14);
+                    }
+                });
+            }
+        });
+    }
+
+    private void updateCardSizes(double width, double height, double labelSize, double meaningSize) {
+        card1Image.setFitWidth(width);
+        card1Image.setFitHeight(height);
+        card2Image.setFitWidth(width);
+        card2Image.setFitHeight(height);
+        card3Image.setFitWidth(width);
+        card3Image.setFitHeight(height);
+
+        // Update font sizes
+        String labelStyle = String.format("-fx-font-size: %.0fpx;", labelSize);
+        String meaningStyle = String.format("-fx-font-size: %.0fpx;", meaningSize);
+
+        time1Label.setStyle(labelStyle);
+        time2Label.setStyle(labelStyle);
+        time3Label.setStyle(labelStyle);
+        meaning1Label.setStyle(meaningStyle);
+        meaning2Label.setStyle(meaningStyle);
+        meaning3Label.setStyle(meaningStyle);
+    }
+    private void initializeCard(ImageView imageView, Image image) {
+        imageView.setImage(image);
+        imageView.setPreserveRatio(true);
+        imageView.setSmooth(true);
+        imageView.setCache(true);
+    }
+
+    @FXML
+    public void toggleHelp() {
+        isHelpVisible = !isHelpVisible;
+        if (isHelpVisible) {
+            mainSplit.setDividerPosition(0, 0.85);
+            helpPanel.setVisible(true);
+        } else {
+            mainSplit.setDividerPosition(0, 1.0);
+            helpPanel.setVisible(false);
         }
-        isMaximized = !isMaximized;
+    }
+
+    @FXML
+    public void handleModeChange() {
+        ReadingMode[] modes = ReadingMode.values();
+        currentMode = modes[(currentMode.ordinal() + 1) % modes.length];
+        updateModeLabels();
+    }
+
+    public void updateModeLabels() {
+        String[] positions = currentMode.getPositions();
+        time1Label.setText(positions[0]);
+        time2Label.setText(positions[1]);
+        time3Label.setText(positions[2]);
+    }
+
+    @FXML
+    public void handleShuffle() {
+        tarotDeck.shuffle();
+        Image cardBack = new Image(getClass().getResourceAsStream("/images/resource/card_back.jpg"));
+        initializeCard(card1Image, cardBack);
+        initializeCard(card2Image, cardBack);
+        initializeCard(card3Image, cardBack);
+        meaning1Label.setText("");
+        meaning2Label.setText("");
+        meaning3Label.setText("");
+    }
+
+    @FXML
+    public void handleOpen() {
+        currentSpread = tarotDeck.drawSpread(3);
+        displayCard(card1Image, meaning1Label, currentSpread.get(0));
+        displayCard(card2Image, meaning2Label, currentSpread.get(1));
+        displayCard(card3Image, meaning3Label, currentSpread.get(2));
+    }
+
+    public void displayCard(ImageView imageView, Label meaningLabel, TarotCard card) {
+        try {
+            Image image = new Image(getClass().getResourceAsStream(card.getImagePath()));
+            imageView.setImage(image);
+            imageView.setRotate(card.getReversed() ? 180 : 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        meaningLabel.setText(card.getName() + (card.getReversed() ? " (Reversed)" : "") +
+                "\n" + card.getMeaning());
     }
 }
